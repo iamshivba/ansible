e3521f5995d74557fda54101d71e373f9e2271bb
iamshivba <iamsnk2209@gmail.com>
2026-02-10 15:54:14 +0530

    Update main.tf
    final draft is ready

--- main.tf
+++ main.tf
@@ -15,15 +15,16 @@ data "aws_ami" "amazon_linux" {
 
 data "aws_key_pair" "my_key" {
       key_name = "shiv_key"
+      include_public_key = true
 }
 
 resource "aws_instance" "ec2_ansible" {
-    ami = "data.aws_ami.amazon_linux"
-    instance_type = "t3.micro "
-    key_name = "aws_key_pair.my_key.key_name
+    ami = "data.aws_ami.amazon_linux.id"
+    instance_type = "t3.micro"
+    key_name = "data.aws_key_pair.my_key.key_name"
 
     tags = {
-    Name ="ansible_master " 
+    Name ="ansible_master" 
    }
 }
 
