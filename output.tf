6f00078f99d2d8fb3442b2be78b558ba361af7c9
iamshivba <iamsnk2209@gmail.com>
2026-02-10 17:27:01 +0530

    Update output.tf
    Add security group

--- output.tf
+++ output.tf
@@ -1,7 +1,14 @@
 output "instance_public_ip" {
-   value = aws_instance.ec2_ansible.public_ip
+   description = "Public IP of all instance"
+   value = aws_instance.ec2_ansible[*].public_ip
 }
 
 output "instance_id" {
-   value = aws_instance.ec2_ansible.id
+   description = "Instance id of all instances"
+   value = aws_instance.ec2_ansible[*].id
+}
+
+output "security_group_id" {
+    description = "Group id of security group"
+    value = aws_security_group.ansible_sg.id
 }
