f80e932b664fb4179c47ee6cd01b73c6c76d3965
iamshivba <iamsnk2209@gmail.com>
2026-02-10 16:05:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "instamce_public_ip" {
-   value = "aws_instance.ec2_ansible.public_ip"
+output "instance_public_ip" {
+   value = aws_instance.ec2_ansible.public_ip
 }
 
 output "instance_id" {
-   value = "aws_instance.ec2_ansible.id"
+   value = aws_instance.ec2_ansible.id
 }
