c5a9cecb033b8d4fe0d3185a7a750e22231264fa
iamshivba <iamsnk2209@gmail.com>
2026-02-10 16:01:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_key_pair" "my_key" {
 }
 
 resource "aws_instance" "ec2_ansible" {
-    ami = "data.aws_ami.amazon_linux.id"
+    ami = data.aws_ami.amazon_linux.id
     instance_type = "t3.micro"
     key_name = "data.aws_key_pair.my_key.key_name"
 
